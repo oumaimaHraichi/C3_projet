837cc76f4206339cd10df3cb5817fe71e6c21539
oumaima <ohraichi@gmail.com>
2019-05-06 11:57:58 +0100

    commitex6

---
+++ projet1/cmake-build-debug/CMakeFiles/projet1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet1.dir/main.c.obj"
+  "projet1.pdb"
+  "projet1.exe"
+  "projet1.exe.manifest"
+  "libprojet1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
